95579a6f20446cde1e360ce6482ec779fbe967bb
Charles Lehong <charles@digitalsolutionfoundry.co.za>
2021-07-08 17:33:40 +0200

    added boiler plate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './views/cart/cart.component';
+import { OrdersComponent } from './views/orders/orders.component';
+import { RegisterComponent } from './views/register/register.component';
+import { LoginComponent } from './views/login/login.component';
+import { WishlistComponent } from './views/wishlist/wishlist.component';
+import { ProductsComponent } from './views/products/products.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    OrdersComponent,
+    RegisterComponent,
+    LoginComponent,
+    WishlistComponent,
+    ProductsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
